0f1bc0c2328b701b64899c72033a2ab2d4f04731
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-05-30 20:00:23 +0300

    Json parser, add boolean

--- example/json/test.php
+++ example/json/test.php
@@ -4,7 +4,7 @@ include __DIR__ . "/Json.php";
 
 $json = new \Jungle\Example\Json();
 var_dump($json->parse('{}'));
-var_dump($json->parse('{"x": 1}'));
+var_dump($json->parse('{"x": 1, "y": true}'));
 var_dump($json->parse('{"x": [1, "hi", {}]}'));
 var_dump($json->parse('{"x": [1, "hi", {}], "b": 76.5}'));
 var_dump($json->parse('{"white in string": 1}'));
