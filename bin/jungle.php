ac0256d7a8b1a3aac6ed85af303cffbc0eae2535
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-06-05 14:27:40 +0300

    Improve bin

--- bin/jungle.php
+++ bin/jungle.php
@@ -2,7 +2,7 @@
 <?php
 
 if (@include __DIR__ . '/../vendor/autoload.php') {
-    if (!@include __DIR__ . '/../../../../autoload.php') {
+    if (!@include __DIR__ . '/../../../autoload.php') {
         throw new \RuntimeException('Cannot load autoload file');
     }
 }
